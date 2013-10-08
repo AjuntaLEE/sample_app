4584d63b42de9e393000f4622c1c8ee80ec9d849
AjuntaLEE <ajunta.lee@mail.com>
2013-10-08 23:31:01 +0200

    finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,8 +13,9 @@ SampleApp::Application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #  get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+  # 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
